6856df5fe29baa28a79f014369de56e0eed7bcf0
hardeylarning <hardeylarning@gmail.com>
2022-07-01 10:52:02 +0100

    jwt-starter

---
+++ src/test/java/com/rocktech/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocktech.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
